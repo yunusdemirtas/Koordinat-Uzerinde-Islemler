70f671ad15be389698cc8dcc261b3877f179a0ec
yunusdemirtas <yunuskmtn@gmail.com>
2022-01-17 02:23:21 +0300

    Koordinat-Ustunde-Islemler
    koordinat düzlemi üstünde üçgen oluşturma, yansıtma ve girilen açıya göre döndürme

---
+++ WindowsFormsApp11/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace WindowsFormsApp11
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 81);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Eksenleri Çiz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(27, 167);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 81);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Üçgen Oluştur";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(27, 281);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(142, 89);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Yansıt(X)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(222, 443);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(189, 443);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Açı";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(27, 395);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(142, 89);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Döndür";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1283, 568);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApp11/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp11
+{
+    public partial class Form1 : Form
+    {
+
+        System.Drawing.Graphics Cizgi;
+        Pen Klm = new Pen(Color.Red, 2);
+        Double[,] UcgenCismi = new Double[3, 4] { { 2, 3,0,1 },{7, 3,0,1 },{2, 6,0,1 } };
+        Double[,] Eksen2BX = new double[2, 2] { { -1.95, 0 }, { 1.95, 0 } };
+        Double[,] Eksen2BY = new double[2, 2] { { 0, 1.95 }, { 0, -1.95 } };
+        Double[,] UcgenYansitXY = new double[4, 4] { { 1,0,0,0 },
+                                                  { 0,-1,0,0 },
+                                                  { 0,0,0,0},
+                                                  { 0,0,0,1 } };
+
+
+        Double[,] YansimisUcgen = new double[3, 4];
+        Double[,] Tdondur = new double[4, 4] { { 1, 0, 0, 0 }, { 0, 1, 0, 0 }, { 0, 0, 1, 0 }, { 0, 0, 0, 1 } };
+
+        int i;
+        int j;
+        int k;
+
+
+
+
+    
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Cizgi = this.CreateGraphics();
+            Cizgi.DrawLine(Klm, KordinatHesaplaX(Eksen2BX[0, 0]), KordinatHesaplaY(Eksen2BX[0, 1]), KordinatHesaplaX(Eksen2BX[1, 0]), KordinatHesaplaY(Eksen2BX[1, 1]));
+            Cizgi.DrawLine(Klm, KordinatHesaplaX(Eksen2BY[0, 0]), KordinatHesaplaY(Eksen2BY[0, 1]), KordinatHesaplaX(Eksen2BY[1, 0]), KordinatHesaplaY(Eksen2BY[1, 1]));
+            Cizgi.Dispose();
+        }
+
+        private int KordinatHesaplaX(Double geciciX)
+        {
+            return Convert.ToInt32(275 + 150 + (100 * geciciX));
+        }
+        private int KordinatHesaplaY(Double geciciY)
+        {
+            return Convert.ToInt32(100 + 150 + (-100 * geciciY));
+        }
+        private int UcgenHesaplaX(Double geciciX)
+        {
+            return Convert.ToInt32(275 + 150 + (25 * geciciX));
+        }
+        private int UcgenHesaplaY(Double geciciY)
+        {
+            return Convert.ToInt32(100 + 150 + (-25 * geciciY));
+        }
+
+        private int YansimisUcgenHesaplaX(Double geciciX)
+        {
+            return Convert.ToInt32(275 + 150 + (25 * geciciX));
+        }
+        private int YansimisUcgenHesaplaY(Double geciciY)
+        {
+            return Convert.ToInt32(100 + 150 + (-25 * geciciY));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Cizgi = this.CreateGraphics();
+            Cizgi.DrawLine(Klm, UcgenHesaplaX(UcgenCismi[0, 0]), UcgenHesaplaY(UcgenCismi[0, 1]), UcgenHesaplaX(UcgenCismi[1, 0]), UcgenHesaplaY(UcgenCismi[1, 1]));
+            Cizgi.DrawLine(Klm, UcgenHesaplaX(UcgenCismi[1, 0]), UcgenHesaplaY(UcgenCismi[1, 1]), UcgenHesaplaX(UcgenCismi[2, 0]), UcgenHesaplaY(UcgenCismi[2, 1]));
+            Cizgi.DrawLine(Klm, UcgenHesaplaX(UcgenCismi[2, 0]), UcgenHesaplaY(UcgenCismi[2, 1]), UcgenHesaplaX(UcgenCismi[0, 0]), UcgenHesaplaY(UcgenCismi[0, 1]));
+            Cizgi.Dispose();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    YansimisUcgen[i, j] = 0;
+                    for (int k = 0; k < 4; k++)
+                    {
+                        YansimisUcgen[i, j] += UcgenCismi[i, k] * UcgenYansitXY[k, j];
+                    }
+                }
+            }
+            Pen Klm = new Pen(Color.Blue, 2);
+            Cizgi = this.CreateGraphics();
+            Cizgi.DrawLine(Klm, YansimisUcgenHesaplaX(YansimisUcgen[0, 0]), YansimisUcgenHesaplaY(YansimisUcgen[0, 1]), YansimisUcgenHesaplaX(YansimisUcgen[1, 0]), YansimisUcgenHesaplaY(YansimisUcgen[1, 1]));
+            Cizgi.DrawLine(Klm, YansimisUcgenHesaplaX(YansimisUcgen[1, 0]), YansimisUcgenHesaplaY(YansimisUcgen[1, 1]), YansimisUcgenHesaplaX(YansimisUcgen[2, 0]), YansimisUcgenHesaplaY(YansimisUcgen[2, 1]));
+            Cizgi.DrawLine(Klm, YansimisUcgenHesaplaX(YansimisUcgen[2, 0]), YansimisUcgenHesaplaY(YansimisUcgen[2, 1]), YansimisUcgenHesaplaX(YansimisUcgen[0, 0]), YansimisUcgenHesaplaY(YansimisUcgen[0, 1]));
+            Cizgi.Dispose();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Double[,] Gecici = new Double[4, 4];
+          
+            Double teta = Convert.ToDouble(textBox1.Text);
+
+            Tdondur[0, 0] = Math.Cos(Math.PI * teta / 180);
+            Tdondur[0, 1] = -(Math.Sin(Math.PI * teta / 180));
+            Tdondur[1, 0] = Math.Sin(Math.PI * teta / 180);
+            Tdondur[1, 1] = Math.Cos(Math.PI * teta / 180);
+
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Gecici[i, j] = 0;
+                    for (int k = 0; k < 4; k++)
+                    {
+                        Gecici[i, j] += UcgenCismi[i, k] * Tdondur[k, j];
+                    }
+                }
+            }
+            Pen Klm = new Pen(Color.Brown, 2);
+            Cizgi = this.CreateGraphics();
+            Cizgi.DrawLine(Klm, YansimisUcgenHesaplaX(Gecici[0, 0]), YansimisUcgenHesaplaY(Gecici[0, 1]), YansimisUcgenHesaplaX(Gecici[1, 0]), YansimisUcgenHesaplaY(Gecici[1, 1]));
+            Cizgi.DrawLine(Klm, YansimisUcgenHesaplaX(Gecici[1, 0]), YansimisUcgenHesaplaY(Gecici[1, 1]), YansimisUcgenHesaplaX(Gecici[2, 0]), YansimisUcgenHesaplaY(Gecici[2, 1]));
+            Cizgi.DrawLine(Klm, YansimisUcgenHesaplaX(Gecici[2, 0]), YansimisUcgenHesaplaY(Gecici[2, 1]), YansimisUcgenHesaplaX(Gecici[0, 0]), YansimisUcgenHesaplaY(Gecici[0, 1]));
+            Cizgi.Dispose();
+        }
+    }
+      
+}
